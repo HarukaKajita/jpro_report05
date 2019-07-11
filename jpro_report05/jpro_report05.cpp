5e614957e262fd653bf27c2a6651c948a04e6446
HarukaKajita <kajitaj63b3@gmail.com>
2019-07-11 21:02:56 +0900

    仕様通りに動くコード完成

--- jpro_report05/jpro_report05.cpp
+++ jpro_report05/jpro_report05.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <fstream>
 using namespace std;
 
 
@@ -7,18 +8,24 @@ private:
 	class BinNode {
 	public:
 		string data;
+		int appearanceNum;
 		BinNode* left;
 		BinNode* right;
 		BinNode(string a = 0, BinNode * b = NULL, BinNode * c = NULL) {
-			data = a; left = b; right = c;
+			data = a; left = b; right = c; appearanceNum = 1;
 		}
-		void printNode() { cout << data << " "; }
+		void printNode() {
+			cout << data << "[" << appearanceNum << "] ";// << endl;
+			//cout << "this = " << this << endl;
+			//cout << "left = " << left << endl;
+			//cout << "right = " << right << endl;
+		}
 	};
 
 	BinNode* root;
 	void traverse(BinNode* rp);
 	BinNode* addNode(BinNode* rp, BinNode* node);
-	BinNode* delNode(BinNode* rp, int x);
+	BinNode* delNode(BinNode* rp, int  appearanceNum);
 public:
 	BinTree() { root = NULL; }
 	void printTree() { traverse(root); }
@@ -26,8 +33,8 @@ public:
 		BinNode* np = new BinNode(str);
 		root = addNode(root, np);
 	}
-	void remove(int x) {
-		root = delNode(root, x);
+	void remove(int apppearanceNum) {
+		root = delNode(root, apppearanceNum);
 	}
 };
 
@@ -42,7 +49,10 @@ BinTree::BinNode* BinTree::addNode(BinNode* rp, BinNode* node) {
 	if (node == NULL) return rp;
 	if (rp == NULL) return node;
 	else {
-		if (node->data <= rp->data) {
+		if (node->data == rp->data) {
+			rp->appearanceNum++;
+		} 
+		else if (node->data < rp->data) {
 			rp->left = addNode(rp->left, node);
 		}
 		else {
@@ -52,48 +62,49 @@ BinTree::BinNode* BinTree::addNode(BinNode* rp, BinNode* node) {
 	}
 }
 
-BinTree::BinNode* BinTree::delNode(BinNode* rp, int x) {
+BinTree::BinNode* BinTree::delNode(BinNode* rp, int appearanceNum) {
 
 	if (rp == NULL) return NULL;
-
-#ifdef DEBUG
-	cout << "del : " << x << endl;
-	cout << "now : " << rp->data << endl;
-	if (rp->left)cout << "left : " << rp->left->data << endl;
-	if (rp->right)cout << "right : " << rp->right->data << endl;
-
-#endif
-	if (x == rp->data) {
+	rp->left = delNode(rp->left, appearanceNum);
+	//cout << rp->data << " : " << rp->appearanceNum << endl;
+	if (appearanceNum == rp->appearanceNum) {
+		//cout << "削除する" << endl;
 		BinNode* left = rp->left;
 		BinNode* right = rp->right;
+		//cout << "right = " << right << endl;
+		//cout << "left = " << left << endl;
 		delete rp;
-		return addNode(right, left);
-	}
-	else {
-		if (x < rp->data) {
-			rp->left = delNode(rp->left, x);
-		}
-		else {
-			rp->right = delNode(rp->right, x);
-		}
+		//cout << "rp = " << rp << " -> ";
+		rp = addNode(right, left);
+		//cout << rp << endl;
+		rp = delNode(rp, appearanceNum);
 		return rp;
 	}
+	else rp->right = delNode(rp->right, appearanceNum);
+	
+	return rp;
 }
 
 int main() {
 	BinTree bt;	// 空の二進木を作成
-	int x;
+	string str;
+	ifstream fin("words.txt");
+	if (!fin) {
+		cerr << "ファイルを開けませんでした。" << endl;
+		exit(EXIT_FAILURE);
+	}
 
-	cout << "正整数をいくつか入力せよ --> ";
-	while (cin >> x && x > 0) {	// 負数が入力されるまで正整数を入力
-		bt.insert(x);	// x をデータとして持つノードを木に追加
+	//cout << "単語をいくつか入力せよ --> ";
+	while (fin >> str) {	// 負数が入力されるまで正整数を入力
+		bt.insert(str);	// x をデータとして持つノードを木に追加
 	}
 
 	bt.printTree();	// bt の木全体を表示する
 	cout << endl;
 
-	while (cout << "削除したい正整数 --> " && cin >> x && x > 0) {
-		bt.remove(x);
+	int appearanceNum = 0;
+	while (cout << "何回出現した単語を削除しまうｓか --> " && cin >> appearanceNum) {
+		bt.remove(appearanceNum);
 		bt.printTree();
 		cout << endl;
 	}
